d216c9f318303932ea5058018391018776d4fd4e
Vetoshkin Alexandr <vetoshkin-av@dartit.ru>
2017-03-02 15:55:43 +0500

    refak

--- src/main/java/pool/PoolConnection.java
+++ src/main/java/pool/PoolConnection.java
@@ -1,6 +1,7 @@
 package pool;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pool.validator.ConnectionValidator;
 import pool.validator.validatorImpl.MsSqlValidator;
 import pool.validator.validatorImpl.MySqlValidator;
@@ -27,7 +28,7 @@ import java.util.concurrent.TimeoutException;
  */
 final class PoolConnection {
     
-    private static final Logger logger = LogManager.getLogger(PoolConnection.class);
+    private static final Logger logger = LoggerFactory.getLogger(PoolConnection.class);
     private final DataSourceProperties prop;
     private final BlockingQueue<Connection> pool = new LinkedBlockingQueue<>();
     private ConnectionValidator validator;
